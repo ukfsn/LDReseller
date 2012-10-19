650e55db0fb66ded42f960640f7440e533c5622c
Jason Clifford <jason@ukfsn.org>
2012-10-19 20:18:58 +0100

    what are the basic admin functions - no actual code in them yet

---
+++ lib/LDReseller/Admin.pm
@@ -0,0 +1,57 @@
+package LDReseller::Admin;
+
+use Dancer ':syntax';
+
+prefix '/admin';
+
+get '/' => sub {
+    template 'admin/index';
+};
+
+get '/*' => sub {
+    my $thing = splat;
+    my @t = schema->resultset($thing)->all();
+    template "admin/$thing", { $thing => \@t };
+};
+
+put '/package' => sub {
+    
+};
+
+put '/vatrate' => sub {
+
+};
+
+put '/service' => sub {
+
+};
+
+put '/invoice' => sub {
+
+};
+
+put '/invoiceline' => sub {
+
+};
+
+put '/payment' => sub {
+
+};
+
+put '/customer' => sub {
+
+};
+
+put '/user' => sub {
+
+};
+
+put '/password' => sub {
+
+};
+
+put '/role' => sub {
+
+};
+
+1;
