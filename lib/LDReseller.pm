384b21912062a4747246826030e8fc02a432596f
Jason Clifford <jason@ukfsn.org>
2012-10-19 17:29:39 +0100

    basic authentication and the admin methods to display things - no views yet

--- lib/LDReseller.pm
+++ lib/LDReseller.pm
@@ -1,27 +1,46 @@
 package LDReseller;
 use Dancer ':syntax';
 use Dancer::Plugin::DBIC 'schema';
+use Dancer::Plugin::Auth::RBAC;
 use Dancer::Plugin::FlashMessage;
+
+use LDReseller::Admin;
 
 our $VERSION = '0.1';
 
-schema->deploy;
+hook 'before' => sub {
+    my %noauth = ();
+    map {$noauth{$_}++} qw{/ /login /logout};
+
+    if (! session('user') && ! $noauth{request->path_info}) {
+        var requested_path => request->path_info;
+        request->path_info('/login');
+    }
+};
 
 get '/' => sub {
     template 'index';
 };
 
+get '/login' => sub {
+    template 'login', { path => vars->{requested_path} };
+};
+
 post '/login' => sub {
     if ( ! params->{login} || ! params->{password} ) {
-        return template 'login';
+        return template 'login', {path => vars->{requested_path}};
     }
     my $auth = auth(params->{login}, params->{password});
-    # XXX Carry on!
-
+    if ( $auth->errors ) {
+        flash error => "Login failed";
+        return template 'login', { path => vars->{requested_path} };
+    }
+    redirect params->{path} || '/';
 };
 
-post '/order' => sub {
-
-};
+get '/logout' => sub {
+    session->destroy;
+    redirect '/';
+}
 
 true;
